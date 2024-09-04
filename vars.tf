2bf9641dd47222473bd9bc06ee3ada2951a4a01a
Jo Schwandke <59925842+j0g3sc@users.noreply.github.com>
2024-09-04 10:45:57 +0200

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -12,3 +12,13 @@ variable "flag" {
   type = bool
   default = false
 }
+
+variable "meshStack_BB_id" {
+  type = string
+  default = ""
+}
+
+variable "meshStack_BB_run_id" {
+  type = string
+  default = ""
+}
